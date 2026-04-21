4e1e75eb797bb340603112740e3608de327c0e30
hunterciraolobaugh-arch <hunterciraolobaugh@brsdstudents.org>
2026-04-21 10:37:38 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-
+// Draw subtle grid lines
+ctx.strokeStyle = "#444";
+ctx.lineWidth = 1;
+for (let i = 0; i < canvas.width; i += 50) {
+    ctx.beginPath(); ctx.moveTo(i, 0); ctx.lineTo(i, canvas.height); ctx.stroke();
+}
+for (let j = 0; j < canvas.height; j += 50) {
+    ctx.beginPath(); ctx.moveTo(0, j); ctx.lineTo(canvas.width, j); ctx.stroke();
+}
